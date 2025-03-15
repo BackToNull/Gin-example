01057619f9b4b28c3d9014c7dbb570111ce2106e
BacktoNull <1227579896@qq.com>
2025-03-15 19:59:28 +0800

    grace restart

--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/BackToNull/Gin-example/routers"
-	"net/http"
-
 	"github.com/BackToNull/Gin-example/pkg/setting"
+	"github.com/BackToNull/Gin-example/routers"
+	"github.com/fvbock/endless"
+	"log"
+	"syscall"
 )
 
 func main() {
-	router := routers.InitRouter()
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPORT)
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPORT),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
